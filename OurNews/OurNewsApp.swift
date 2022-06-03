479df294a6c8f41d23903bacd1b0ea6e4c471dd5
mukul372 <mukul@scogo.in>
2022-06-03 17:34:49 +0530

    Initial Commit

---
+++ OurNews/OurNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurNewsApp.swift
+//  OurNews
+//
+//  Created by Mukul Jangir on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct OurNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
